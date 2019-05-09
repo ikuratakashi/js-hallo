11d2073e26ed1e7f941dac151d76661097a17534
ikuratakashi <ikura1108@icloud.com>
2019-05-09 18:36:35 +0900

    onclick追加

--- www/js/hello.js
+++ www/js/hello.js
@@ -1,5 +1,16 @@
+class test {
+    constructor( pId, pAge ) {
+        this.id = pId;
+        this.age = pAge;
+        this.ele = document.getElementById(this.id);
+        this.ele.onclick = this.onClick;
+    }
+    onClick(){
+        alert("click!!");
+    }
+}
+ 
 function onLoad(){
-    var a;
-    a = 0;
-    alert("hello");
+    //インスタンスを生成する
+    var objTest = new test('btn01', 41);
 }
